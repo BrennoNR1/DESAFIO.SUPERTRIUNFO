94a3d9285a3ff997bb2a8ec1b9b7df3dddc14bdb
BrennoNR1 <brennonascimento460@gmail.com>
2025-08-25 21:51:14 +0000

    Criei o printf

---
+++ supertrunfo.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+int main() {
+   // Carta 1
+
+    char carta1[20] = "Carta 1";
+    printf("%s:\n", carta1);
+
+    char estado1[20] = "Rio Grande do Sul";
+    printf("Estado: %s\n", estado1);
+
+    char codigo1[20] = "A-001";
+    printf("Código: %s\n", codigo1);
+
+    char cidade1[20] = "Porto Alegre";
+    printf("Cidade: %s\n", cidade1);
+
+    float populacao1 = 1.4;
+    printf("População: %.1f milhões\n", populacao1);   
+
+    float area1 = 281730.0;
+    printf("Área: %.2f km²\n", area1);
+
+    float pib1 = 81.5;
+    printf("PIB: %.2f bilhões\n", pib1); 
+
+    int pontos_turisticos1 = 25;
+    printf("Pontos turísticos: %d\n\n", pontos_turisticos1);
+
+    // Carta 2
+    
+    char carta2[20] = "Carta 2";
+    printf("%s:\n", carta2);
+
+    char estado2[20] = "São Paulo";
+    printf("Estado: %s\n", estado2);
+
+    char codigo2[20] = "A-002";
+    printf("Código: %s\n", codigo2);
+
+    char cidade2[20] = "São Paulo";
+    printf("Cidade: %s\n", cidade2);
+
+    float populacao2 = 12.3;
+    printf("População: %.1f milhões\n", populacao2);   
+
+    float area2 = 121400.0;
+    printf("Área: %.2f km²\n", area2);
+
+    float pib2 = 450.0;
+    printf("PIB: %.2f bilhões\n", pib2); 
+
+    int pontos_turisticos2 = 50;
+    printf("Pontos turísticos: %d\n", pontos_turisticos2);
+
+    return 0;
+}
